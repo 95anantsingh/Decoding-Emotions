d58d265d707e56dc3f125be46ccb4fcf59166a27
Anant <95anantsingh@gmail.com>
2023-03-04 23:16:18 -0500

    summary csvs

--- utils/history_push.sh
+++ utils/history_push.sh
@@ -1,10 +1,11 @@
 echo -e "\nGetting a session...\n"
 srun -c2 --mem="2GB" -t1:00:00 --pty /bin/bash  -c "
 source ~/.bashrc;
+
 echo -e '\nGoogle Drive Sync';
-rclone sync '/home/as14229/NYU_HPC/Multilingual-Speech-Emotion-Recognition-System/history/Zips' 'nyu:Data Science/MSERS-Histories' -P;
+rclone sync '/home/as14229/NYU_HPC/Multilingual-Speech-Emotion-Recognition-System/summary/zips' 'nyu:Data Science/MSERS-Histories' -P;
 
 echo -e '\nOneDrive Sync';
-rclone sync '/home/as14229/NYU_HPC/Multilingual-Speech-Emotion-Recognition-System/history/Zips' 'OneDrive:MSERS-Histories' -P;
+rclone sync '/home/as14229/NYU_HPC/Multilingual-Speech-Emotion-Recognition-System/summary/zips' 'OneDrive:MSERS-Histories' -P;
 "
 echo -e "\nDone\n"
