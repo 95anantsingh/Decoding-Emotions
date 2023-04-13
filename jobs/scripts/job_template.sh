1a2e89fefb05fe37dc9dd35291803a2dbbd406bd
Anant <95anantsingh@gmail.com>
2023-04-13 01:54:30 -0400

    differential scores and mixing weights plot

--- jobs/scripts/job_template.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-module purge
-source ~/.bashrc
-conda activate NLP_Nightly
-cd /home/as14229/NYU_HPC/Multilingual-Speech-Emotion-Recognition-System/
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
-
-python extracter.py
--- jobs/scripts/schedule_jobs.sh
+++ jobs/scripts/schedule_jobs.sh
@@ -8,8 +8,3 @@ sbatch /home/as14229/NYU_HPC/Multilingual-Speech-Emotion-Recognition-System/jobs
 sbatch /home/as14229/NYU_HPC/Multilingual-Speech-Emotion-Recognition-System/jobs/sbatch/job6.sbatch
 sbatch /home/as14229/NYU_HPC/Multilingual-Speech-Emotion-Recognition-System/jobs/sbatch/job7.sbatch
 sbatch /home/as14229/NYU_HPC/Multilingual-Speech-Emotion-Recognition-System/jobs/sbatch/job8.sbatch
-sbatch /home/as14229/NYU_HPC/Multilingual-Speech-Emotion-Recognition-System/jobs/sbatch/job9.sbatch
-sbatch /home/as14229/NYU_HPC/Multilingual-Speech-Emotion-Recognition-System/jobs/sbatch/job10.sbatch
-sbatch /home/as14229/NYU_HPC/Multilingual-Speech-Emotion-Recognition-System/jobs/sbatch/job11.sbatch
-sbatch /home/as14229/NYU_HPC/Multilingual-Speech-Emotion-Recognition-System/jobs/sbatch/job12.sbatch
-sbatch /home/as14229/NYU_HPC/Multilingual-Speech-Emotion-Recognition-System/jobs/sbatch/job13.sbatch
